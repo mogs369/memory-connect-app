ae86e1ecf7918dbe71273824f372d379a6a2ba21
mogs369 <140217391+mogs369@users.noreply.github.com>
2025-03-11 20:05:33 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+```jsx
+import React from 'react';
+import MemoryConnectApp from './components/MemoryConnectApp';
+import './App.css';
+
+function App() {
+  return (
+    
+      
+    
+  );
+}
+
+export default App;
+```
